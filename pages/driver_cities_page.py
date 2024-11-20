8ff1acbddb963b21509578fc965f2d004268890c
arturotrejo <a.trejog@hotmail.com>
2024-11-20 17:02:26 -0500

    Refactor some locators in driver_cities_page

--- pages/driver_cities_page.py
+++ pages/driver_cities_page.py
@@ -4,19 +4,20 @@ from pages.base_page import BasePage
 
 
 class DriverCitiesPage(BasePage):
-    CITIES_FOOTER_BUTTON = (By.XPATH, "//h2[text()='DRIVER']//ancestor::div/ul//span[@data-testid='core-ui-text'][.='Cities']")
+    CITIES_FOOTER_BUTTON = (By.CSS_SELECTOR, "[aria-label='Footer links'] a[href='/driver/cities']")
     SEARCH_FIELD = (By.XPATH, "//input[@data-testid='SearchInput']")
-    FIRST_SEARCH_OPTION = (By.XPATH, "//ul[@role='listbox']/li[@role='option']//span")
+    SEARCH_OPTION = (By.XPATH, "//ul[@role='listbox']/li[@role='option']")
     PHONE_FIELD = (By.XPATH, "//input[@data-testid='phone']")
-    AGREE_TO_TERMS_CHECKBOX = (By.XPATH, "//input[@data-testid='core-ui-checkbox']")
+    AGREE_TO_TERMS_CHECKBOX = (By.CSS_SELECTOR, "input[name='tos']")
     APPLY_TO_DRIVER_BUTTON = (By.XPATH, "//button//span[text()='Apply to drive']")
 
     def go_to_cities_page(self):
         self.driver.find_element(*self.CITIES_FOOTER_BUTTON).click()
 
     def search_and_select_city(self, city_name):
+        city_option = (By.XPATH, f"//*[contains(text(), '{city_name}')]")
         self.type_text(self.SEARCH_FIELD, city_name)
-        self.driver.find_element(*self.FIRST_SEARCH_OPTION).click()
+        self.driver.find_element(*self.SEARCH_OPTION).find_element(*city_option).click()
 
     def enter_phone_number_and_apply(self, phone_number):
         self.wait_for_element_to_be_visible(self.PHONE_FIELD)
